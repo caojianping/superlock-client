182dc70d3fca2610251394842af37bf450d8c52f
caojianping <caojianping1107@163.com>
2020-04-13 10:16:50 +0800

    修改锁仓详情页面标题格式。

--- superlock-mobile/src/views/lock/lock-detail/lock-detail.vue
+++ superlock-mobile/src/views/lock/lock-detail/lock-detail.vue
@@ -5,8 +5,7 @@
         <div class="lock-detail-body">
             {{ ((lockProjectObj = lockProject || {}), void 0) }}
             <h1 class="lock-detail-title">
-                锁仓宝 -
-                {{ lockProjectObj.length + unitTypes[lockProjectObj.unit - 1] }}
+                {{ `BCB锁仓 - ${lockProjectObj.length}${unitTypes[lockProjectObj.unit - 1]}` }}
             </h1>
 
             <div class="lock-detail-intro lock-container">
