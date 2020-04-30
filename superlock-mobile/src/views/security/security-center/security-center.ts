12dc0e3d8bc00a1bd7fc2936eb7da2bd4214788d
caojianping <caojianping1107@163.com>
2020-04-30 19:21:05 +0800

    移动端：修复安全中心进入资金密码页面没有添加来源缓存的BUG。

--- superlock-mobile/src/views/security/security-center/security-center.ts
+++ superlock-mobile/src/views/security/security-center/security-center.ts
@@ -1,7 +1,9 @@
 import Vue from 'vue';
 import { namespace } from 'vuex-class';
 import { Component } from 'vue-property-decorator';
+
 import TYPES from '@/store/types';
+import { Token } from '@/ts/common';
 import { UserInfoModel } from '@/ts/models';
 
 import { Icon, CellGroup, Cell } from 'vant';
@@ -18,6 +20,14 @@ export default class SecurityCenter extends Vue {
     @userModule.Mutation(TYPES.SET_STATES) setStates!: (payload: any) => any;
     @userModule.Mutation(TYPES.CLEAR_STATES) clearStates!: () => any;
     @userModule.Action('fetchUserInfo') fetchUserInfo!: () => any;
+
+    goFund() {
+        Token.setFundFrom('/security/center');
+        this.$router.push({
+            path: '/security/fund/password',
+            query: { from: '/security/center' }
+        });
+    }
 
     mounted() {
         this.fetchUserInfo();
--- superlock-mobile/src/views/security/security-center/security-center.vue
+++ superlock-mobile/src/views/security/security-center/security-center.vue
@@ -5,7 +5,7 @@
         <CellGroup class="scb-separator">
             <Cell title="UID" :value="userInfo.userId || '--'" />
             <Cell title="登录密码" is-link :to="{ path: '/security/login/password', query: { from: '/security/center' } }" />
-            <Cell title="资金密码" is-link :to="{ path: '/security/fund/password', query: { from: '/security/center' } }" />
+            <Cell title="资金密码" is-link @click="goFund" />
             <Cell v-if="!userInfo.email" title="邮箱绑定" is-link :to="{ path: '/security/email', query: { from: '/security/center' } }">
                 <span class="unbind">未绑定</span>
             </Cell>
