3c1163427dbb8140276475c44e04310434022790
caojianping <caojianping1107@163.com>
2020-05-18 16:28:06 +0800

    移动端：修改模态框样式、邀请好友页面样式。

--- superlock-mobile/src/components/mine/invite-prompt/invite-prompt.vue
+++ superlock-mobile/src/components/mine/invite-prompt/invite-prompt.vue
@@ -1,6 +1,6 @@
 <template>
     <Modal class="invite-prompt" v-model="isShow" :closeable="false" :closeOnClickOverlay="false">
-        <template slot="title">
+        <template slot="header">
             <div class="invite-prompt-title clearfix">
                 <img src="../../../assets/images/reminder.png" alt="温馨提示" />
                 <div>
@@ -10,12 +10,11 @@
             </div>
         </template>
 
-        <ul>
-            <li>您需要先设置好友相关利率的默认值后，才能邀请好友哦！</li>
-            <li>
-                <Button class="effect-shadow" type="primary" round block @click="confirm">立即设置</Button>
-            </li>
-        </ul>
+        <p>您需要先设置好友相关利率的默认值后，才能邀请好友哦！</p>
+
+        <template slot="footer">
+            <Button class="effect-shadow" type="primary" round block @click="confirm">立即设置</Button>
+        </template>
     </Modal>
 </template>
 
